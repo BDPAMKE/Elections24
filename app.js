27b68ba0b8519137b42104f81bdff5e0d029b735
mvbrahm <55369617+mvbrahm@users.noreply.github.com>
2024-06-08 17:57:17 +0000

    electioninfo

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var registerRouter = require('./routes/register');
 var dashboardRouter = require('./routes/dashboard');
+var electionInfoRouter = require('./routes/getelectioninfo');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dashboard', dashboardRouter);
 app.use('/register', registerRouter);
+app.use('/getelectioninfo', electionInfoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
