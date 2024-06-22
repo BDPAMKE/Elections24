7a3e76e3a6339fd9fe6eedf423a0f3f7cf9d0ca1
RamenXk <158761011+RamenXk@users.noreply.github.com>
2024-06-22 15:40:21 +0000

    finnaly have an updating navbar

--- routes/getelectioninfo.js
+++ routes/getelectioninfo.js
@@ -15,7 +15,7 @@ const auth = require("../middleware/verifyToken");
 
 // Generic route to get a list of users from the beginning (first 100)
 router.get('/', auth, function(req,res,next) {
-    const url = 'https://elections-irv.api.hscc.bdpa.org/v1/info'
+    const url = 'https://elections-cpl.api.hscc.bdpa.org/v1/info'
     const token = process.env.BEARER_TOKEN;
     //console.log(url); //Debug
   
